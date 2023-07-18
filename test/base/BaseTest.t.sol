52441ae8f880071944db8ecba6d1dc1202016182
bigq <99990370+yum0e@users.noreply.github.com>
2023-07-18 16:56:05 +0200

    feat: add fork tests using cheatsheet contract as example (#2)

---
+++ test/base/BaseTest.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {IAddressesProvider} from "src/SismoConnectLib.sol";
+
+interface IAvailableRootsRegistry {
+  event RegisteredRoot(uint256 root);
+
+  function registerRoot(uint256 root) external;
+
+  function owner() external view returns (address);
+}
+
+contract BaseTest is Test {
+  IAddressesProvider sismoAddressesProvider =
+    IAddressesProvider(0x3Cd5334eB64ebBd4003b72022CC25465f1BFcEe6);
+  IAvailableRootsRegistry availableRootsRegistry;
+
+  function _registerTreeRoot(uint256 root) internal {
+    // get availableRootsRegistry from the sismoAddressesProvider
+    availableRootsRegistry = IAvailableRootsRegistry(
+      sismoAddressesProvider.get("sismoConnectAvailableRootsRegistry")
+    );
+    address rootsRegistryOwner = availableRootsRegistry.owner();
+    // prank to the rootsRegistryOwner
+    vm.startPrank(rootsRegistryOwner);
+    availableRootsRegistry.registerRoot(root);
+    vm.stopPrank();
+  }
+}
